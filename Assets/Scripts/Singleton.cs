0dc8c99802c3c74c085712f223a6d0f4e2f8c51f
jos <aluno@naverecife.local>
2018-10-01 17:50:20 -0300

    DesignPatterns

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+	private static Singleton instance;
+
+	public static Singleton GetInstance{
+		get{
+			if(instance == null){
+				instance = GameObject.FindObjectOfType<Singleton>();
+			}
+			return instance;
+		}
+	}
+}
