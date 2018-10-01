426d84ca4f621028fa1a5eb2e37f33b9732c6b7e
trash <aluno@naverecife.local>
2018-10-01 19:01:26 -0300

    v1 bibi

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+    public Vector2 playerPos;
+
+
+
+    void Start () {
+      player = Singleton.GetInstance.player;
+        playerPos = new Vector2(0, -4);
+       // Instantiate()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
