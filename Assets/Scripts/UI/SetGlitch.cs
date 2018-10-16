03e17375db574be09803ab842be50f5cb0cd2e2e
Aluno <aluno@naverecife.local>
2018-10-16 18:53:50 -0300

    menu indo

---
+++ Assets/Scripts/UI/SetGlitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGlitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
